1b6b554b0851377e9843a6d3698b0c58284caa59
nasik-dh <nasikdhiu@gmail.com>
2025-05-31 14:54:27 +0530

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -1,1 +1,19 @@
+// models/Article.js
+const mongoose = require('mongoose');
 
+const ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
